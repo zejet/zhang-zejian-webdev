29269e8d37ed8727adfb5ca7bc0582d330b3116c
zejet <steven@Zejians-MacBook-Pro.local>
2017-07-09 17:51:26 -0400

    undo unnecessary changes

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//app.set('view engine', 'ejs');
-//require('./utilities/filelist');
+app.set('view engine', 'ejs');
+require('./utilities/filelist');
 
 app.use(app.express.static(__dirname + '/public'));
 
